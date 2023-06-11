f3a60ff69e7fdb7ff83ed937df0b83c713510837
hannusia <hanna.yershova@ucu.edu.ua>
2023-06-11 03:54:46 +0300

    add uuid to tasks database

--- tasks/app.py
+++ tasks/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.utils import secure_filename
 import os
@@ -12,14 +12,8 @@ app.config['SECRET_KEY'] = 'your_secret_key'
 
 db = SQLAlchemy(app)
 
-
-
-client = hazelcast.HazelcastClient(
-    cluster_name="tasks", 
-)
-
-mapp = client.get_map("tasks").blocking() 
-
+client = hazelcast.HazelcastClient(cluster_name="tasks")
+mapp = client.get_map("tasks").blocking()
 
 
 class Task(db.Model):
@@ -27,14 +21,23 @@ class Task(db.Model):
     title = db.Column(db.String(100), nullable=False)
     description = db.Column(db.Text, nullable=True)
     picture = db.Column(db.String(255), nullable=True)
+    user_id = db.Column(db.Integer, nullable=False)
 
-@app.route('/')
+
+@app.route('/', methods=['GET'])
 def index():
-    tasks = Task.query.all()
-    return render_template('index.html', tasks=tasks)
+    user_uuid = request.args.get('uuid')
+    session['uuid'] = user_uuid
+    print(f"User UUID: {user_uuid}")
+    tasks = Task.query.filter_by(user_id=user_uuid).all()
+    return render_template('index.html', tasks=tasks, user_uuid=session.get('uuid'))
+
+
 
 @app.route('/add', methods=['GET', 'POST'])
 def add():
+    user_uuid = session.get('uuid')
+
     if request.method == 'POST':
         title = request.form['title']
         description = request.form['description']
@@ -42,19 +45,24 @@ def add():
 
         if picture:
             filename = secure_filename(picture.filename)
-            picture.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            save_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            picture.save(save_path)
         else:
             filename = None
 
-        new_task = Task(title=title, description=description, picture=filename)
+        if user_uuid is None:
+            # Handle unauthorized access
+            return redirect(url_for('index', uuid=user_uuid))
+
+        new_task = Task(title=title, description=description, picture=filename, user_id=user_uuid)
         db.session.add(new_task)
         db.session.commit()
 
-        mapp.put(new_task.id, new_task)
+        return redirect(url_for('index', uuid=user_uuid))
 
-        return redirect(url_for('index'))
+    return render_template('add.html', user_uuid=user_uuid)
 
-    return render_template('add.html')
+
 
 @app.route('/delete/<int:task_id>', methods=['GET', 'POST'])
 def delete(task_id):
@@ -64,12 +72,12 @@ def delete(task_id):
         db.session.delete(task)
         mapp.remove(task_id)
         db.session.commit()
-        return redirect(url_for('index'))
+        return redirect(url_for('index', uuid=session.get('uuid')))
 
     return render_template('delete.html', task=task)
 
-if __name__ == '__main__':
 
+if __name__ == '__main__':
     with app.app_context():
         db.create_all()
     app.run(debug=True)
